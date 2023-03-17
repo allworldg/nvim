d3b351d450abddc9773d319ba1bb6af797fc15a5
allworldg <944937881@qq.com>
2023-03-17 13:49:41 +0800

    nvim-tree: add 'open directory auto open nvim-tee,open file not auto

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -8,11 +8,23 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- when open the neovim ,auto open the nvim-tree
-local function open_nvim_tree()
+-- when open the neovim directory ,auto open the nvim-tree
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
+
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 vim.keymap.set({ 'n', 'i' }, '<A-m>', '<cmd>:NvimTreeToggle<CR>', {})
