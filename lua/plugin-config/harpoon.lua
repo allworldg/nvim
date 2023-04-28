30bed1719764d9d631f7c4e0604e27af68f609b6
allworldg <944937881@qq.com>
2023-04-28 11:34:44 +0800

    update harpoon keymap

--- lua/plugin-config/harpoon.lua
+++ lua/plugin-config/harpoon.lua
@@ -7,7 +7,7 @@ end
 harpoon.setup({
   
 })
-vim.keymap.set("n","<leader>hl",require("harpoon.ui").toggle_quick_menu,{})
+vim.keymap.set("n","<c-m>",require("harpoon.ui").toggle_quick_menu,{})
 vim.keymap.set("n","<leader>ha",require("harpoon.mark").add_file,{})
 vim.keymap.set("n","<c-h>",require("harpoon.ui").nav_prev,{silent=true})
 vim.keymap.set("n","<c-l>",require("harpoon.ui").nav_next,{silent=true})
