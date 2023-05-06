f93531ed55a1819096c757e21ac1d52226291e4c
allworldg <944937881@qq.com>
2023-05-06 17:48:00 +0800

    add indent_blankline; update harpoon keybinding

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -156,8 +156,18 @@ require("lazy").setup({
   },
   {
     'ThePrimeagen/harpoon',
-    config=function ()
+    config = function()
       return require("plugin-config.harpoon")
     end
-  }
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end
+  },
 })
