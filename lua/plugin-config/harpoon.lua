cb2a4414f12a29bd5f2b2afa0dcb1e468c3fe0e7
allworldg <944937881@qq.com>
2023-05-23 11:25:50 +0800

    update harpoon version and add harpoon tabline

--- lua/plugin-config/harpoon.lua
+++ lua/plugin-config/harpoon.lua
@@ -5,9 +5,15 @@ if not ok then
 end
 
 harpoon.setup({
-
+  tabline = true
 })
 vim.keymap.set("n", "<A-p>", require("harpoon.ui").toggle_quick_menu, {})
 vim.keymap.set("n", "<A-a>", require("harpoon.mark").add_file, {})
 vim.keymap.set("n", "<c-h>", require("harpoon.ui").nav_prev, { silent = true })
 vim.keymap.set("n", "<c-l>", require("harpoon.ui").nav_next, { silent = true })
+
+vim.cmd('highlight! HarpoonInactive guibg=#ececec guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=whilte guifg=#000000')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=#ececec guifg=#7aa2f7')
+vim.cmd('highlight! TabLineFill guibg=#f3f3f3 guifg=white')
