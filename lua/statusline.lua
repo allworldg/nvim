62fb98ee3bb7ce3047ec622d26efaec097892fbe
allworldg <944937881@qq.com>
2025-01-17 17:23:44 +0800

    update statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,7 +1,16 @@
-vim.cmd [[
-      highlight mywhite guifg=#ffffff guibg=#ffffff
-      set statusline=
-      set statusline+=%#PmenuSel#%-10(%t%) "left section
-      set statusline+=%#mywhite#%=         "middle section
-      set statusline+=%=%#PmenuSel#%-6.(%l%)%-3.(%p%) "right section
-]]
+-- vim.cmd [[
+--       highlight mywhite guifg=#ffffff guibg=#ffffff
+--       set statusline=
+--       set statusline+=%#PmenuSel#%-10(%t%) "left section
+--       set statusline+=%#mywhite#%=         "middle section
+--       set statusline+=%=%#PmenuSel#%-6.(%l%)%-3.(%p%) "right section
+-- ]]
+vim.cmd([[highlight mywhite guifg=#ffffff guibg=#ffffff]])
+
+local leftSectionColor = "%#PmenuSel#"
+local leftSection = leftSectionColor .. "%-10(%t%)"
+local middleSectionColor = "%#mywhite#"
+local middleSection = middleSectionColor .. "%="
+local rightSectionColor = "%#PmenuSel#"
+local rightSection = "%=" .. rightSectionColor .. "%-6.(%l%)%-3.(%p%)"
+vim.opt.statusline = leftSection .. middleSection .. rightSection
