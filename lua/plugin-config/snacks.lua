3e7e9bb4687102f373194c3243c300510fc1c90d
allworldg <944937881@qq.com>
2025-01-18 23:59:36 +0800

    finally delete noice.nvim

--- lua/plugin-config/snacks.lua
+++ lua/plugin-config/snacks.lua
@@ -14,7 +14,7 @@ return {
   opts = {
     -- words = {enabled = true},
     dim = {},
-    notifier={},
+    -- notifier={},
     dashboard = {
       preset = {
         header = [[allworldg]]
@@ -24,7 +24,7 @@ return {
       -- refer to the configuration section below
       sections = {
         { section = "startup" },
-        { section = "header",padding=1},
+        { section = "header", padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
