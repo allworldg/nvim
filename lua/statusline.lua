faef08d579eb6292ac43c2a039c1fcf3e63ad034
allworldg <944937881@qq.com>
2025-01-23 20:53:08 +0800

    delete comment in statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,10 +1,3 @@
--- vim.cmd [[
---       highlight mywhite guifg=#ffffff guibg=#ffffff
---       set statusline=
---       set statusline+=%#PmenuSel#%-10(%t%) "left section
---       set statusline+=%#mywhite#%=         "middle section
---       set statusline+=%=%#PmenuSel#%-6.(%l%)%-3.(%p%) "right section
--- ]]
 vim.cmd([[
 highlight mywhite guifg=#ffffff guibg=#ffffff
 highlight leftSectionColor guibg=#0087af guifg= #ffffff 
@@ -18,4 +11,5 @@ local middleSection = middleSectionColor .. "%="
 local rightSectionColor = "%#rightSectionColor#"
 local rightSection = "%=" .. rightSectionColor .. "%-6.(%l%)%-3.(%p%)"
 vim.opt.statusline = leftSection .. middleSection .. rightSection
+-- keep statusline be full of whole tab, not in every window
 vim.o.laststatus = 3
