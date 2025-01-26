67af36d00dca880eedeed1dd34d1034d10dec2df
allworldg <944937881@qq.com>
2025-01-26 11:00:05 +0800

    update snacks,update fzf-lua config:add some highlight and winopts config

--- lua/plugin/fzf-lua.lua
+++ lua/plugin/fzf-lua.lua
@@ -12,21 +12,61 @@ return {
         ["bg+"] = { "bg", "CursorLine" }, -- the horizontal one.
         ["fg+"] = { "fg", "Normal" },     -- the Normal of horizontal one.
         ["gutter"] = { "bg", "Normal" },  -- the vertical one.
+        ["hl"] = "#0550ae:bold",
+        ["hl+"] = "#0550ae:bold"
       },
       winopts = {
-        fullscreen = true,
+        height = 1,
+        fullscreen = false,
         backdrop = 100, --fully transparent
         preview = {
-          title = false
+          title = false,
+          layout = "vertical"
         }
       },
       keymap = {
-        fzf = {
-        }
+        builtin = {
+          ["<C-j>"] = "preview-down",
+          ["<C-k>"] = "preview-up", },
       },
       files = {
         cwd_prompt = false
       },
     }
+    local profile_files = {
+      winopts = {
+        preview = {
+          hidden = true
+        }
+      },
+    }
+    local profile_live_grep = {
+      winopts = {
+        preview = {
+          vertical = "down:60%",
+          border = { "", "", "", "│", "", "", "", "│" },
+        }
+      }
+    }
+
+    -- change highlight color to head, e.g:"<ctrl-g>" under the input area
+    vim.cmd [[highlight FzfLuaHeaderBind guifg=#0969da]]
+    -- change highlight color to head, e.g:"Fuzzy Search" under the input area
+    vim.cmd [[highlight FzFLuaHeaderText guifg=#0969da]]
+    -- vim.cmd [[highlight IncSearch guifg=#000000]]
+    -- change highlight in live_grep user input text
+    vim.cmd [[highlight  FzFLuaLivePrompt guifg=#000000 gui=bold]]
+
+
+    local opt = {
+      silent = true,
+      noremap = true,
+    }
+    vim.keymap.set("n", "<C-f>", function() require 'fzf-lua'.live_grep(profile_live_grep) end, opt)
+    vim.keymap.set("n", "<C-p>", function() require 'fzf-lua'.files(profile_files) end, opt)
+    vim.keymap.set("n", "gr", ":FzfLua lsp_references<CR>", opt)
+    vim.keymap.set("n", "gi", ":FzfLua lsp_implementations<CR>", opt)
+    vim.keymap.set("n", "gd", ":lua require('fzf-lua').lsp_definitions({ jump_to_single_result = true })<CR>", opt)
+    vim.keymap.set("n", "gD", ":FzfLua lsp_declarations<CR>", opt)
   end
 }
