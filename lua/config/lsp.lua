acd8a0e369be2e30deb472697f694f9954ea240c
allworldg <944937881@qq.com>
2025-04-29 15:23:06 +0800

    feat(lsp): make lsp config to one file

---
+++ lua/config/lsp.lua
@@ -0,0 +1,256 @@
+-----------basic config -----------------------
+local signature_help = vim.lsp.buf.signature_help
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.signature_help = function()
+  return signature_help({
+    border = "single",
+  })
+end
+
+local hover_help = vim.lsp.buf.hover
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.buf.hover = function()
+  return hover_help({
+    border = "single"
+  })
+end
+
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = ""
+    },
+    numhl = {
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+    }
+  },
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = "minimal",
+    border = "single",
+    source = true,
+    header = "",
+    prefix = "",
+  },
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local bufnr = event.buf
+    local opts = { buffer = bufnr }
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    vim.keymap.set('n', '<leader>se', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>k', function()
+      vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+      vim.api.nvim_create_autocmd("CursorMoved", {
+        group = vim.api.nvim_create_augroup("virtual-lines", { clear = true }),
+        callback = function()
+          vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+          return true
+        end
+      })
+    end, opts)
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts) -- use by other tools
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts) -- use by other tools
+    -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    -- vim.keymap.set('n', '<leader>wl', function()
+    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    --use same keybinding by fzflua.references
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- use conform.nvim to format
+    -- vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+  end
+})
+
+---------- lsp config -----------------------------
+
+vim.lsp.config.emmet = {
+  cmd = { 'emmet-language-server', '--stdio' },
+  filetypes = {
+    'css',
+    'eruby',
+    'html',
+    'vue',
+    'htmldjango',
+    'javascriptreact',
+    'less',
+    'pug',
+    'sass',
+    'scss',
+    'typescriptreact',
+    'htmlangular',
+  },
+  root_markers = { '.git' },
+}
+vim.lsp.enable("emmet", true)
+
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+vim.lsp.config.html = {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  filetypes = { 'html', 'templ'},
+  root_markers = { 'package.json', '.git' },
+  settings = {},
+  init_options = {
+    provideFormatter = true,
+    embeddedLanguages = { css = true, javascript = true },
+    configurationSection = { 'html', 'css', 'javascript' },
+  },
+}
+vim.lsp.enable('html',true)
+
+vim.lsp.config.jsonls = {
+  cmd = { 'vscode-json-language-server', '--stdio' },
+  filetypes = { 'json', 'jsonc' },
+  init_options = {
+    provideFormatter = true,
+  },
+  root_markers = { '.git',"tsconfig.json", "package.json", "jsconfig.json"},
+}
+vim.lsp.enable('jsonls', true)
+
+vim.lsp.config.lua = {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = {
+    ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+    ".git",
+  },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files and plugins
+        library = { vim.env.VIMRUNTIME, "${3rd}/luv/library" },
+        checkThirdParty = false,
+      },
+    },
+  },
+}
+vim.lsp.enable("lua")
+
+local mason_registry = require("mason-registry")
+local vue_language_path = mason_registry.get_package("vue-language-server"):get_install_path() ..
+    "/node_modules/@vue/language-server"
+vim.lsp.config.ts_ls = {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { "vue", "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  root_markers = {
+    "tsconfig.json", "package.json", "jsconfig.json", ".git"
+  },
+  init_options = {
+    hostInfo = 'neovim',
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_path,
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+}
+vim.lsp.enable("ts_ls", false)
+
+
+
+local mason_registry = require("mason-registry")
+local vue_language_server = mason_registry.get_package("vue-language-server"):get_install_path() ..
+    "/node_modules/@vue/language-server"
+vim.lsp.config.vtsls = {
+  cmd = { "vtsls", "--stdio" },
+  filetypes = { "vue", "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  root_markers = {
+    "tsconfig.json", "package.json", "jsconfig.json", ".git"
+  },
+  settings = {
+    complete_function_calls = true,
+    vtsls = {
+      enableMoveToFileCodeAction = true,
+      autoUseWorkspaceTsdk = true,
+      experimental = {
+        maxInlayHintLength = 30,
+        completion = {
+          enableServerSideFuzzyMatch = true,
+        },
+      },
+      tsserver = {
+        globalPlugins = {
+          {
+            name = '@vue/typescript-plugin',
+            location = vue_language_server,
+            languages = { 'vue' },
+            configNamespace = "typescript",
+            enableForWorkspaceTypeScriptVersions = true,
+          }
+        }
+      }
+    },
+    typescript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      suggest = {
+        completeFunctionCalls = true,
+      },
+      inlayHints = {
+        enumMemberValues = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        variableTypes = { enabled = false },
+      },
+    },
+    javascript = {
+      updateImportsOnFileMove = { enabled = "always" },
+    }
+  },
+}
+vim.lsp.enable("vtsls",true)
+
+local mason_registry = require("mason-registry")
+local typescript_path = mason_registry.get_package("vue-language-server"):get_install_path() ..
+    "/node_modules/typescript/lib"
+vim.lsp.config.volar = {
+  cmd = { "vue-language-server", "--stdio" },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  root_markers = { "package.json" },
+  init_options = {
+    vue = {
+      hybridMode = true,
+    },
+    typescript = {
+      tsdk = typescript_path
+    },
+  },
+
+}
+vim.lsp.enable("volar",true)
--- lua/config/lsp/jsonls.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.config.jsonls = {
-  cmd = { 'vscode-json-language-server', '--stdio' },
-  filetypes = { 'json', 'jsonc' },
-  init_options = {
-    provideFormatter = true,
-  },
-  root_markers = { '.git',"tsconfig.json", "package.json", "jsconfig.json"},
-}
-vim.lsp.enable('jsonls', true)
