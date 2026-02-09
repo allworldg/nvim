5ee02144db869958077cf1f399f603884417ae37
allworldg <944937881@qq.com>
2026-02-09 21:40:22 +0800

    feat(gitsigns): add gitsigns

--- lua/plugin/indent-blankline.lua
+++ lua/plugin/indent-blankline.lua
@@ -1,25 +1,22 @@
 return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    cond = false,
-    main = "ibl",
-    config = function()
-      require("ibl").setup {
-        -- indent = {
-        --   highlight = {
-        --     "CursorColumn",
-        --     "Whitespace",
-        --   }
-        -- },
-        -- scope = { enabled = false },
-        -- whitespace = {
-        --   highlight = {
-        --     "Whitespace",
-        --   },
-        --   remove_blankline_trail = true,
-        -- },
-      }
-    end
-  },
+  "lukas-reineke/indent-blankline.nvim",
+  cond = false,
+  main = "ibl",
+  config = function()
+    require("ibl").setup {
+      -- indent = {
+      --   highlight = {
+      --     "CursorColumn",
+      --     "Whitespace",
+      --   }
+      -- },
+      -- scope = { enabled = false },
+      -- whitespace = {
+      --   highlight = {
+      --     "Whitespace",
+      --   },
+      --   remove_blankline_trail = true,
+      -- },
+    }
+  end
 }
-
