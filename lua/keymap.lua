8f5124fe26a0029b26dee4cf6107a0b4c7ac1a66
allworldg <944937881@qq.com>
2026-03-10 21:49:32 +0800

    fix(keymap): delete some unused keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,7 +7,7 @@
 --   command_mode = "c",
 
 
--- leader key 为空
+-- leader key is space
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 local opt = {
@@ -23,17 +23,14 @@ map("v", "$", "g_", opt)
 map("v", "g_", "$", opt)
 map("n", "$", "g_", opt)
 map("n", "g_", "$", opt)
--- save and exit
+
 map("n", "<leader>w", ":w<CR>", opt)
 map("n", "<leader>wq", ":w|q<CR>", opt)
-map("n","<leader>wa", ":wa<CR>",opt)
-
--- -- exit
+map("n", "<leader>wa", ":wa<CR>", opt)
 map("n", "q", "<Nop>", opt)
 map("n", "qq", ":q!<CR>", vim.tbl_extend('force', opt, { nowait = true }))
 map("n", "<leader>q", ":qa<CR>", opt)
 
--- fix :set wrap
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
@@ -41,26 +38,11 @@ vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 map("n", "<c-u>", "10k", opt)
 map("n", "<c-d>", "10j", opt)
 
--- visual模式下缩进代码
 map("v", "<", "<gv", opt)
 map("v", ">", ">gv", opt)
 
 map("v", "p", 'P', opt)
--- buffer
-map("n", "<leader>bl", ":buffers<CR>:buffer<Space>", {})
 
 vim.keymap.set("n", "<c-h>", ":tabp<CR>", { silent = true })
 vim.keymap.set("n", "<c-l>", ":tabn<CR>", { silent = true })
 vim.keymap.set("n", "<leader>ta", ":tabnew<CR>", { silent = true })
-
-
-
--- ------------------------------------------------------------------
--- -- windows 分屏快捷键
--- ------------------------------------------------------------------
--- 左右比例控制
-map("n", "<c-left>", ":vertical resize -2<cr>", opt)
-map("n", "<c-right>", ":vertical resize +2<cr>", opt)
--- 上下比例
-map("n", "<c-down>", ":resize +2<cr>", opt)
-map("n", "<c-up>", ":resize -2<cr>", opt)
