6ada2ccb3b985e7ea89dfe78e6d40ae3bce37f04
allworldg <944937881@qq.com>
2026-04-01 10:58:46 +0800

    fix(emmylua_ls): try to add global settings to enable vim check

--- lsp/emmylua_ls.lua
+++ lsp/emmylua_ls.lua
@@ -7,5 +7,17 @@ return {
     '.luacheckrc',
     '.git',
   },
-  workspace_required = true,
+  workspace_required = false,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files and plugins
+        library = { vim.env.VIMRUNTIME, "${3rd}/luv/library" },
+        checkThirdParty = false,
+      },
+    },
+  },
 }
