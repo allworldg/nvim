a4cd3f1eb8bbacd8310e3757ef0f3abfe2eb37f5
allworldg <944937881@qq.com>
2026-04-30 17:52:55 +0800

    feat(vim.pack): replace lazy with pack

---
+++ colors/allworldg-light.lua
@@ -0,0 +1,155 @@
+vim.cmd.highlight 'clear'
+if vim.fn.exists 'syntax_on' == 0 then
+  vim.cmd.syntax 'reset'
+end
+
+vim.g.colors_name = 'allworldg-vlight'
+
+
+local palette = {
+  bg = '#ffffff',
+  fg = '#1f1f1f',
+
+  purple = '#af00db',
+  black = '#1f1f1f',
+  blue = '#0857b5',
+  red = '#d1242f',
+  green = '#026B0A',
+  cyan = '#216f86',
+  yellow = '#9a6700',
+  white = '#ececec',
+
+  bright_blue = '#dae9f9',
+  bright_black = '#8c8c8c',
+  bright_white = '#f6f8fa',
+
+  boolean = '#0c40d2',
+  bright_yellow = '#fce094',
+  fg_function = '#795e26',
+  fg_string = '#a31515',
+}
+
+local ui = {
+  bg = palette.bg,
+  bg_float = palette.bright_white,
+  fg = palette.fg,
+  fg_muted = palette.bright_black,
+  fg_comment = palette.bright_black,
+  border = palette.border,
+
+}
+
+---@type table<string, vim.api.keyset.highlight>
+local groups = vim.tbl_extend('error', {}, {
+  ---===BaseUI===
+  Added = { fg = palette.green },
+  Normal = { fg = ui.fg, bg = ui.bg },
+  NormalFloat = { fg = ui.fg, bg = ui.bg_float },
+  NormalNc = { fg = ui.fg, bg = ui.bg },
+  Cursor = { fg = ui.bg, bg = ui.fg },
+  CursorLine = { bg = palette.bright_blue },
+  CursorLineNr = { fg = ui.fg, bg = ui.bg },
+  LineNr = { fg = ui.fg_muted, bg = ui.bg },
+  ColorColumn = { bg = palette.bright_black },
+  Comment = { fg = ui.fg_comment, bg = ui.bg, cterm = { nocombine = true }, nocombine = true },
+  Pmenu = { fg = ui.fg, bg = ui.bg_float },
+  PmenuThumb = { bg = palette.bright_black },
+  PmenuMatch = { fg = palette.blue, bold = true, cterm = { bold = true } },
+  PmenuSel = { bg = palette.bright_blue },
+  PmenuMatchSel = { fg = palette.blue, bold = true, cterm = { bold = true } },
+  EndOfBuffer = { fg = palette.bright_white },
+
+  WinSeparator = { fg = ui.fg_muted },
+  -- StatusLine = { fg = colors.bg, bg = colors.statusLineBg, cterm = { reverse = true } },
+  -- StatusLineNC = { fg = colors.bright_black, bg = colors.bright_white, cterm = { reverse = true } },
+
+  Visual = { bg = palette.white },
+  Search = { bg = palette.bright_yellow, fg = ui.fg },
+  IncSearch = { fg = ui.fg, bg = palette.bright_yellow },
+  MoreMsg = { fg = ui.fg },
+  ErrorMsg = { fg = palette.red },
+  WarningMsg = { fg = palette.yellow },
+  OkMsg = { fg = palette.green },
+  Question = { fg = palette.fg },
+
+  --===Builtin Syntax===
+  Constant = { fg = palette.green },
+  Directory = { fg = palette.fg },
+  Delimiter = { fg = palette.fg, ctermfg = 0 },
+  Function = { fg = palette.fg_function },
+  Identifier = { fg = palette.fg },
+  Keyword = { fg = palette.purple, bold = false, cterm = { bold = false } },
+  Operator = { fg = palette.fg },
+  PreProc = { fg = palette.blue },
+  String = { fg = palette.fg_string },
+  Statement = { fg = palette.purple, bold = false },
+  Special = { fg = palette.fg },
+  Type = { fg = palette.cyan },
+  Title = { fg = palette.fg, bold = true },
+  MatchParen = { bg = palette.bright_blue },
+
+  --===Treesitter===
+  ['@Variable'] = { fg = palette.blue },
+  ['@boolean'] = { fg = palette.boolean },
+  ['@type.builtin'] = { fg = palette.cyan },
+  ['@type.builtin.go'] = { fg = palette.cyan },
+  ['punctuation.bracket'] = { link = 'Delimiter' },
+  ['@lsp.type.macro'] = { link = 'keyword' },
+  rustKeyWord = { link = 'keyword' },
+  ['@tag'] = { fg = palette.green },
+
+
+  --===Diagnostic===
+  DiagnosticHint = { fg = palette.bright_black },
+  DiagnosticWarn = { fg = palette.yellow },
+  DiagnosticError = { fg = palette.red },
+  DiagnosticInfo = { fg = palette.fg },
+  DiagnosticFloatingHint = { link = 'DiagnosticHint' },
+  DiagnosticFloatingWarn = { link = 'DiagnosticWarn' },
+  DiagnosticFloatingError = { link = 'DiagnosticError' },
+  DiagnosticfloatingInfo = { link = 'DiagnosticInfo' },
+  DiagnosticUnderlineHint = { link = 'DiagnosticHint' },
+  DiagnosticUnderlineWarn = { link = 'DiagnosticWarn' },
+  DiagnosticUnderlineError = { link = 'DiagnosticError' },
+  DiagnoticUnderlineInfo = { link = 'DiagnosticInfo' },
+
+  ---statusline
+  StatusLine = { bg = palette.bg },
+  StatuslineFileName = { bg = palette.bright_blue, fg = palette.blue },
+  StatuslineGit = { bg = palette.blue, fg = palette.bright_white },
+  StatuslineLsp = { bg = palette.bg, fg = palette.bright_white },
+  StatuslineMiddle = { bg = palette.bg },
+  StatuslineFileType = { fg = palette.blue, bg = palette.bg },
+  StatuslinePercentage = { fg = palette.blue, bg = palette.bright_blue },
+  StatuslineLocation = { fg = palette.bg, bg = palette.blue },
+  StatuslineInactive = { bg = palette.bright_white },
+  StatuslineDisabled = { bg = palette.bg },
+
+  ---Tabline
+  TabLine = { bg = palette.white },
+  TabLineSel = { bg = palette.bg, bold = true },
+
+
+  ---Blink.cmp
+  BlinkCmpMenuSelection = { link = 'PmenuSel' },
+  BlinkCmpLabelMatch = { link = 'PmenuMatchSel' },
+
+  ---NvimTree---
+  NvimTreeNormal = { bg = palette.bright_white, fg = palette.fg },
+  NvimTreeCursorLine = { bg = palette.bright_blue },
+  NvimTreeGitNewIcon = { fg = palette.green },
+  NvimTreeGitDeletedIcon = { fg = palette.red },
+  NvimTreeGitDirtyIcon = { fg = palette.yellow },
+  --fzflua---
+  FzfLuaHeaderBind = { fg = palette.blue },
+  FzfLuaHeaderText = { fg = palette.blue },
+  FzfLuaLivePrompt = { fg = palette.black, bold = true },
+  FzfLuaBufNr = { fg = palette.yellow },
+  --DocHighlight---
+  DocHighlight = { bg = palette.bright_blue }
+
+})
+
+for group, opts in pairs(groups) do
+  vim.api.nvim_set_hl(0, group, opts);
+end
--- init.lua
+++ init.lua
@@ -1,46 +1,10 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+-----plugin
+vim.pack.add({{src="git@github.com:kylechui/nvim-surround.git"}},{conform=false})
+require("nvim-surround").setup()
+------------------------------------baseConfig------------------
+vim.cmd.colorscheme('allworldg-light')
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-        require("nvim-surround").setup({
-        })
-      end,
-    },
-    { 'projekt0n/github-nvim-theme', name = 'github-theme' }
-  },
-  -- automatically check for plugin updates
-  checker = { enabled = false },
-})
-------------------------------------baseConfig------------------
-vim.cmd.colorscheme('github_light')
 -- VSCode extension
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
